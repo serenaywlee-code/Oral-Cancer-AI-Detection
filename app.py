2eef5aa7ced542f9a015d98603309b2e19086199
Serena Lee <serena.yw.lee@gmail.com>
2026-01-20 10:14:51 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,13 @@
 import os
 import tensorflow as tf
 import streamlit as st
-import gdown
 
 @st.cache_resource
 def load_model():
     model_path = "oral_cancer_model_optimized.tflite"
 
-    # OPTIONAL: Google Drive download (recommended for big files)
-    FILE_ID = "PASTE_YOUR_GOOGLE_DRIVE_FILE_ID_HERE"
-
     if not os.path.exists(model_path):
-        st.info("⬇️ Downloading AI model...")
-        gdown.download(
-            f"https://drive.google.com/uc?id={FILE_ID}",
-            model_path,
-            quiet=False
-        )
-
-    if not os.path.exists(model_path):
-        st.error("❌ Model file not found.")
+        st.error("❌ Model file not found. Make sure oral_cancer_model_optimized.tflite is uploaded.")
         st.stop()
 
     interpreter = tf.lite.Interpreter(model_path=model_path)
